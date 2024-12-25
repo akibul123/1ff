078db953d91b2ea40d1c268f8aba0b1f59e83826
Fahim Islam <160403765+akibul123@users.noreply.github.com>
2024-12-25 14:28:39 +0600

    Create 00fun2.js

---
+++ scripts/commands/00fun2.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+  name: "00fun2",
+  version: "2.0.0",
+  permission: 0,
+  credits: "nayan",
+  description: "",
+  prefix: false,
+  category: "user",
+  usages: "",
+  cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("@Khalid Bin Oalid")==0 || (event.body.indexOf("@Khalid Bin Oalid")==0 || (event.body.indexOf("I love u")==0 || (event.body.indexOf("I love you")==0)))) {
+		var msg = {
+				body: "please দয়া করে কোন ছেলে মেনশন দেবেন না 🙏🙏🙂
+ এখন khalid bin oalid কচি কচি মেয়ে পটাতে বিজি আছে 😁😘🥰))"
+    }
+			api.sendMessage(msg, threadID, messageID);
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+}
